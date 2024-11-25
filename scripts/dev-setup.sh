0e49dc10c9f951aebff1e2788c46309641940d82
rvujji <rvujji@users.noreply.github.com>
2024-11-25 15:54:08 +0530

    Update dev-setup.sh

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -3,8 +3,10 @@
 docker --version
 mkdir D:/docker/mongo-data
 docker pull mongo:latest
+# windows
 docker run -d --name mongodb-container -p 27017:27017 -v D:/docker/mongo-data:/data/db mongo:latest
-
+# linux
+# docker run -d --name mongodb-container -p 27017:27017 -v ~/mongo-data:/data/db mongo:latest
 ## FastAPI docker setup
 
 ## Nodejs docker setup
