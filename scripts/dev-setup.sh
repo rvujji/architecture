4e9324318b9de4a94e7d76384ad0c680546906e8
rvujji <rvujji@users.noreply.github.com>
2024-11-25 15:36:10 +0530

    Update dev-setup.sh

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -1,1 +1,10 @@
+## Setup Servers
+## DB docker setup
+docker --version
+docker pull mongo:latest
+docker run -d --name mongodb-container -p 27017:27017 mongo:latest
+
+## FastAPI docker setup
+
+## Nodejs docker setup
 
